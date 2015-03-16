764bbf79b082ca3bea76e2c1809eb1cf85b59b56
Richard Samuels <richard.l.samuels@gmail.com>
2015-03-16 04:03:42 -0400

    As shown in video

---
+++ video5/better-scales.js
@@ -0,0 +1,28 @@
+//Hard coded scale values
+var x = d3.scale.linear()
+		.domain([0,350]
+		.range([padding,width]);
+
+
+//Have d3 calculate the domain for you!
+var maxY = d3.max(data, function (d, i ) {
+	return parseInt(d.x);
+});
+
+var minY = d3.min(data, function (d, i ) {
+	return parseInt(d.x);
+}
+
+//Now a scale that dynamically modifies itself even when the data changes
+var x2 = d3.scale.linear()
+		.domain([minY,maxY]
+		.range([padding,width]);
+
+//Even faster than running min and max seperately 
+var extents = d3.extents(data, function (d, i ) {
+	return parseInt(d.x);
+}
+
+var x3 = d3.scale.linear()
+		.domain([extents[0],extents[1]]
+		.range([padding,width]);
